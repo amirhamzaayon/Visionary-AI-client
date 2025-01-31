40786d42cabd0fba5cf7164dd115640917091eab
amirhamzaayon <amirhamzaayon.official@gmail.com>
2025-01-31 23:41:52 +0600

    add popup window for review comment in users my post dashboard

--- src/Components/Dashboard - User/MyPost.jsx
+++ src/Components/Dashboard - User/MyPost.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 export const MyPost = () => {
+  const [close, setClose] = useState(false);
   return (
     <div className="min-h-screen p-4 bg-white rounded-xl">
       <div className="flex flex-row justify-between p-2 bg-gray-100 rounded-lg">
@@ -17,7 +20,7 @@ export const MyPost = () => {
       <div>
         <h3 className="p-4 text-xl ">Mangae Your Post Activity</h3>
         <hr className="h-px bg-gray-300 border-0" />
-        <div className="flex flex-col gap-4 my-4">
+        <div className="relative flex flex-col w-full gap-4 my-4 ">
           <div className="overflow-x-auto border rounded-box border-base-content/5 bg-base-100 ">
             <table className="table">
               {/* head */}
@@ -41,7 +44,9 @@ export const MyPost = () => {
                   </td>
                   <td>🔥10 </td>
                   <td className="space-x-2 ">
-                    <button className="btn">Review Comments</button>
+                    <button onClick={() => setClose(!close)} className="btn">
+                      Review Comments
+                    </button>
                   </td>
                   <td>
                     <button className="btn">Delete</button>
@@ -50,6 +55,67 @@ export const MyPost = () => {
               </tbody>
             </table>
           </div>
+          {close ? (
+            <div className="absolute flex items-center justify-center w-full h-fit backdrop-blur-sm">
+              <div
+                onClick={() => setClose(!close)}
+                className="absolute btn right-2 top-2"
+              >
+                Close
+              </div>
+              <div className="overflow-y-auto bg-gray-100 border h-96 rounded-box border-base-content/5">
+                <table className="table">
+                  {/* head */}
+                  <thead>
+                    <tr>
+                      <th></th>
+                      <th>User Name</th>
+                      <th>Comment</th>
+                      <th>Your Feedback</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* row 1 */}
+                    <tr>
+                      <th>1</th>
+                      <td>
+                        <p>Commentor Name</p> <p>comentor@email.com</p>
+                      </td>
+                      <td>
+                        <p>Showing Comment</p>
+                      </td>
+                      <td>
+                        <p className="rounded-full badge">Null</p>
+                      </td>
+
+                      <td>
+                        <div className="dropdown">
+                          <div tabIndex={0} role="button" className="m-1 btn">
+                            Take Action
+                          </div>
+                          <ul
+                            tabIndex={0}
+                            className="p-2 shadow-sm dropdown-content menu bg-base-100 rounded-box z-1 w-52"
+                          >
+                            <li>
+                              <a>Depreciating</a>
+                            </li>
+                            <li>
+                              <a>Disparaging</a>
+                            </li>
+                            <li>
+                              <a>Backlashes</a>
+                            </li>
+                          </ul>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
